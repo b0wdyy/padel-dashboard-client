139c1949818d605ac00872f13e7632ba09468e40
b0wdyy <vandaelbodhi@hotmail.com>
2024-11-21 01:45:26 +0100

    feat: route to products with id in query

--- app/components/ui/dashboard/dashboard-card.tsx
+++ app/components/ui/dashboard/dashboard-card.tsx
@@ -1,16 +1,28 @@
+import { Link } from '@remix-run/react'
+
 interface DashboardCardProps {
+  href: string
   title: string
   amount: number
   icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
 }
 
-export function DashboardCard({ title, amount, icon }: DashboardCardProps) {
+export function DashboardCard({
+  href,
+  title,
+  amount,
+  icon,
+}: DashboardCardProps) {
   const Icon = icon
+
   return (
-    <div className="bg-stone-700 p-8 rounded-md relative overflow-hidden">
+    <Link
+      to={href}
+      className="bg-stone-700 p-8 rounded-md relative overflow-hidden"
+    >
       <h2 className="text-xs text-gray-200 mb-2 opacity-75">{title}</h2>
       <p className="text-8xl font-bold">{amount}</p>
       <Icon className="absolute -bottom-8 -right-8 w-72 fill-stone-600 -rotate-12" />
-    </div>
+    </Link>
   )
 }
--- app/routes/_dashboard.dashboard.tsx
+++ app/routes/_dashboard.dashboard.tsx
@@ -1,6 +1,7 @@
 import { Category } from '@prisma/client'
 import { LoaderFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import { useCallback } from 'react'
 
 import { DashboardCard } from '~/components/ui/dashboard/dashboard-card'
 import { ShoeIcon } from '~/components/ui/icons/shoe'
@@ -21,12 +22,33 @@ export const loader: LoaderFunction = async () => {
 export default function Dashboard() {
   const categories = useLoaderData<typeof loader>()
 
-  function getAmount(category: string): number {
-    return (
-      categories.find((c: Category) => c.name === category)?.products.length ??
-      0
-    )
-  }
+  const getCategory = useCallback(
+    (category: string) => {
+      return categories.find((c: Category) => c.name === category)
+    },
+    [categories],
+  )
+
+  const getAmount = useCallback(
+    (category: string) => {
+      return (
+        categories.find((c: Category) => c.name === category)?.products
+          .length ?? 0
+      )
+    },
+    [categories],
+  )
+
+  const createQueryString = useCallback(
+    (category: string) => {
+      const params = new URLSearchParams()
+      const categoryUuid = getCategory(category).uuid
+      params.set('uuid', categoryUuid)
+
+      return params.toString()
+    },
+    [getCategory],
+  )
 
   return (
     <div>
@@ -35,16 +57,19 @@ export default function Dashboard() {
       <div className="mt-8 grid grid-cols-3 gap-8">
         <DashboardCard
           title="Rackets"
+          href={`/products?${createQueryString('Rackets')}`}
           amount={getAmount('Rackets')}
           icon={TennisRacketIcon}
         />
         <DashboardCard
           title="Shoes"
+          href={`/products?${createQueryString('Shoes')}`}
           amount={getAmount('Shoes')}
           icon={ShoeIcon}
         />
         <DashboardCard
           title="Balls"
+          href={`/products?${createQueryString('Balls')}`}
           amount={getAmount('Balls')}
           icon={TennisBallIcon}
         />
